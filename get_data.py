dc225716df6210cca8ab050f736d4d7743c973b5
mnoroozi <3pid.mollanoroozi@gmail.com>
2020-07-01 10:14:37 +0430

    from mongodb

--- get_data.py
+++ get_data.py
@@ -1,23 +1,34 @@
 import csv
+from pymongo import MongoClient
 
-with open('news_data.csv','r') as userFile:
-    userFileReader = csv.reader(userFile)
-    for row in userFileReader:
-        print(row)
-
-
-title = "سلام عنوان"
-url = "سلام لینک"
-sections = "سلام دسته بندی ها"
-summary = "سلام خلاصه"
-date = "سلام تاریخ"
-code = "سلام کد خبر"
-tags = "سلام برچسبها"
-text = "سلام متن"
-row_contents = [title, url, sections, summary, date, code, tags, text]
+# with open('news_data.csv','r') as userFile:
+#     userFileReader = csv.reader(userFile)
+#     for row in userFileReader:
+#         print(row)
 
 
-# with open('news_data.csv', 'a') as data_file:
-#     newFileWriter = csv.writer(data_file)
-#     newFileWriter.writerow(row_contents)
+# title = "سلام عنوان"
+# url = "سلام لینک"
+# sections = "سلام دسته بندی ها"
+# summary = "سلام خلاصه"
+# date = "سلام تاریخ"
+# code = "سلام کد خبر"
+# tags = "سلام برچسبها"
+# text = "سلام متن"
+# row_contents = [title, url, sections, summary, date, code, tags, text]
+
+
+
+
+client = MongoClient()
+db = client['newsdb_week']
+articles = db.weekarticles
+
+myquery = { "url": { "$regex": "https://namehnews.com/" } }
+for a in articles.find(myquery):
+    row_contents = [a["title"], a["url"], a["sections"], a["summary"], a["date"],
+                    a["code"], a["tags"], a["text"]]
+    with open('news_data.csv', 'a') as data_file:
+        newFileWriter = csv.writer(data_file)
+        newFileWriter.writerow(row_contents)
 
